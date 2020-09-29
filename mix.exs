2fc6cc6dfce1ae9c5ad739912d8358bf0bc2aeb2
Rafał Studnicki <studzien@gmail.com>
2020-09-29 09:34:23 +0200

    Add tool versions

--- mix.exs
+++ mix.exs
@@ -11,14 +11,12 @@ defmodule Codegraph.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:jason, "~> 1.1"}
